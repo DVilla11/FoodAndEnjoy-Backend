58e9ee711569729e43534125f7b1cc074c779a8a
DVilla11 <david97villaj@gmail.com>
2022-06-06 19:31:20 +0200

    Estableciendo relación y cambio nombre ids

--- src/main/java/com/dvj/foodandenjoy/model/dao/entity/ComidaEntity.java
+++ src/main/java/com/dvj/foodandenjoy/model/dao/entity/ComidaEntity.java
@@ -2,19 +2,23 @@ package com.dvj.foodandenjoy.model.dao.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
-/*@Getter @Setter
+@Getter @Setter
 @Entity
 @Table(name="T_COMIDA")
 public class ComidaEntity {
 
 	@Id
-	private int id;
+	private int Comida_id;
 	
+	@ManyToOne
+	@JoinColumn(name="Restaurante_id", nullable=false)
 	private RestauranteEntity restaurante;
 	
 	private String nombreComida;
@@ -25,4 +29,4 @@ public class ComidaEntity {
 	
 	private String imagen;
 	
-}*/
+}
--- src/main/java/com/dvj/foodandenjoy/model/dao/entity/RestauranteEntity.java
+++ src/main/java/com/dvj/foodandenjoy/model/dao/entity/RestauranteEntity.java
@@ -1,13 +1,15 @@
 package com.dvj.foodandenjoy.model.dao.entity;
 
-import java.util.LinkedList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -21,7 +23,7 @@ public class RestauranteEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private int Restaurante_id;
 	
 	@Column(nullable = false)
 	private String nombreRestaurante;
@@ -50,6 +52,9 @@ public class RestauranteEntity {
 	
 	@Column(nullable = false)
 	private String contraseña;
+	
+	@OneToMany(mappedBy = "Comida_id", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<ComidaEntity> comida;
 	
 	@Transient
 	private String TokenJWT;
